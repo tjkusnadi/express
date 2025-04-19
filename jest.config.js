3ce50318ff649237f513194cb6e5f8f265788804
Tj <42162660+tjkusnadi@users.noreply.github.com>
2025-04-19 12:14:08 +0700

    test: add coverage (#8)

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/test/**/*.test.ts']
+  testMatch: ['**/test/**/*.test.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
